13a25acdbb07abea1c62e796238e33b1333aa47d
placeholder <placeholder>
2023-11-23 16:31:30 -0800

    r

--- machine_learning/rust_python_modules/rust_tokenizer/src/lib.rs
+++ machine_learning/rust_python_modules/rust_tokenizer/src/lib.rs
@@ -4,13 +4,13 @@ use pyo3::types::PyString;
 
 /// Tokenizer object to tokenize Python strings into a list of characters
 #[pyclass]
-pub struct Tokenizer {}
+pub struct CharacterLevelTokenizer {}
 
 #[pymethods]
-impl Tokenizer {
+impl CharacterLevelTokenizer {
     #[new]
     pub fn new() -> Self {
-        Tokenizer {}
+        CharacterLevelTokenizer {}
     }
 
     /// Takes a Python string and returns a list of characters
@@ -21,9 +21,36 @@ impl Tokenizer {
     }
 }
 
+#[pyclass]
+pub struct SentencePieceTokenizer {
+    #[pyo3(get)]
+    pub model_path: String,
+    pub vocab: HashMap<String, i64>,
+    pub unk_token: String,
+    pub unk_token_id: i64,
+    pub pad_token: String,
+    pub pad_token_id: i64,
+}
+
+// #[pymethods]
+// impl SentencePieceTokenizer {
+//     #[new]
+//     pub fn new(model_path: String) -> Self {
+//         SentencePieceTokenizer { model_path }
+//     }
+
+//     /// Takes a Python string and returns a list of tokens
+//     pub fn tokenize(&self, text: &PyString) -> PyResult<Vec<String>> {
+//         // todo
+
+
+//         Ok(vec![]);
+//     }
+// }
+
 /// A Python module implemented in Rust.
 #[pymodule(rust_tokenizer)]
 fn tokenizer(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_class::<Tokenizer>()?;
+    m.add_class::<CharacterLevelTokenizer>()?;
     Ok(())
 }
