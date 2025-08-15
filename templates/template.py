828aa92e24476a0ce379a3c7c8b6d50ad16484f9
concl <107962474+concl@users.noreply.github.com>
2025-08-14 22:08:03 -0700

    alphazero update and template update

--- templates/template.py
+++ templates/template.py
@@ -6,6 +6,13 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
 
+# sklearn
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.linear_model import LinearRegression, Lasso
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder
+
 # Tools
 from einops import rearrange
 from tqdm.auto import tqdm
